ae7ad9ae9982db3f8944be1812e2bf625b1ffeb1
mrcoder57 <tiwariji2300@gmail.com>
2024-09-19 23:06:27 +0530

    update import for mmap

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-import Map from "./component/dialog/map";
+import dynamic from 'next/dynamic';
+
+const Map = dynamic(() => import("./component/dialog/map"), { ssr: false });
 
 export default function App() {
+  if (typeof window !== "undefined") {
+    return (
+      <div>
+        <Map/>
+      </div>
+    )
+  }
   return (
-    <Map />
+    
+    <Map/>
   );
 }
